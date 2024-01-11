38a11cdde33ad69973be395787c4e5a93713f237
Spencer <spencerlelswick@gmail.com>
2024-01-10 20:43:23 -0500

    Adds service logic to get/handle user accounts data

--- client/src/app/board-user/board-user.component.ts
+++ client/src/app/board-user/board-user.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { UserService } from '../services/user.service';
+import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-board-user',
   templateUrl: './board-user.component.html',
   styleUrl: './board-user.component.css',
 })
-export class BoardUserComponent {}
+export class BoardUserComponent implements OnInit {
+
+  userData = {}
+
+  constructor(private userService: UserService, private storageService: StorageService) {
+
+  }
+
+  ngOnInit(): void {
+    this.loadUserData()
+  }
+
+  loadUserData() {
+    let storedUser = this.storageService.getUser()
+    this.userService.getUserData(storedUser.id).subscribe((data: any) => {
+      this.userData = data
+    })
+  }
+}
